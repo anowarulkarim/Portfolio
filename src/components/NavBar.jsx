673c5b492ee21d314d9cb8961e3b314f1ec08b65
anowarulkarim <anowarulkarim8@gmail.com>
2024-01-28 13:08:15 +0600

    Full name added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -29,7 +29,7 @@ const NavBar=()=>{
         <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
             
             <div>
-                <h1 className="text-4xl font-extralight">Anowarul karim</h1>
+                <h1 className="text-4xl font-extralight">Bhuyan Md Anowarul karim</h1>
             </div>
             <ul className="hidden md:flex">
             {links.map(({id,link})=>(
